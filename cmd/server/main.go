15ef912123158db5b814fb7f0fc8c54239402e73
Lapanxd <morgan.dumas@outlook.fr>
2025-10-14 21:09:16 +0200

    feat: add db connexion + user model + save user in db on the register route

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,56 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"github.com/lapanxd/volatus-api/internal/models"
 	"github.com/lapanxd/volatus-api/internal/routes"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
+func SetupDatabase() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dns := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
+	)
+	db, err := gorm.Open(postgres.Open(dns), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Error connecting to database", err)
+	}
+
+	log.Println("Connected to database")
+	return db
+}
+
 func main() {
+	db := SetupDatabase()
+	
+	err := db.AutoMigrate(&models.User{})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	r := gin.Default()
 
+	authGroup := r.Group("/auth")
+
 	routes.HealthRoutes(r)
+	routes.AuthRoutes(authGroup, db)
 
 	r.Run(":8080")
 
--- internal/routes/auth.go
+++ internal/routes/auth.go
@@ -4,7 +4,9 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lapanxd/volatus-api/internal/models"
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 type RegisterInput struct {
@@ -12,11 +14,13 @@ type RegisterInput struct {
 	Password string `json:"password" binding:"required"`
 }
 
-func AuthRoutes(r *gin.RouterGroup) {
-	r.POST("/register", Register)
+func AuthRoutes(r *gin.RouterGroup, db *gorm.DB) {
+	r.POST("/register", func(c *gin.Context) {
+		Register(c, db)
+	})
 }
 
-func Register(c *gin.Context) {
+func Register(c *gin.Context, db *gorm.DB) {
 	var input RegisterInput
 
 	if err := c.ShouldBindJSON(&input); err != nil {
@@ -26,19 +30,23 @@ func Register(c *gin.Context) {
 
 	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot create user"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot hash password"})
 		return
 	}
 
-	user := map[string]string{
-		"username": input.Username,
-		"password": string(hashedPassword),
+	user := models.User{
+		Username: input.Username,
+		Password: string(hashedPassword),
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Cannot create user"})
 	}
 
 	c.JSON(http.StatusOK, gin.H{
 		"message": "User created",
 		"user": gin.H{
-			"username": user["username"],
+			"username": user.Username,
 		},
 	})
 }
